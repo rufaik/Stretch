475b20258b6c9e6a386bbb6e1b9adc4e4f888dde
Kems <rufai@hotmail.co.uk>
2019-11-02 12:39:25 +0000

    a

--- src/Home.js
+++ src/Home.js
@@ -23,9 +23,9 @@ const Home = () => {
     <div className="pt5">
     <div className="tc">
 
-     <div style = {{display: 'flex', justifyContent: 'center', margin: '0 auto'}} className = "pb3 mb3 w2  pa2 pa3-ns ml1 ml7-ns mr1 mr7-ns">
+     <div size={400} style = {{display: 'flex', justifyContent: 'center', width: 'fit-content', margin: '0 auto'}} className = "title pb3 mb3 pa2 pa3-ns ml1 ml7-ns mr1 mr7-ns">
     <img  alt='robots' 
-      src='./stt.png' />
+      src='./stt.png' width="600"/>
  
   </div>
 
